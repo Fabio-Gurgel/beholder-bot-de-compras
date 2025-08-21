49d7e8579aa6f28f7c1d1a7101aadd08a950baec
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2025-08-21 19:13:28 -0300

    Utilizando o Stopwatch para monitorar tempo de execução do robô

--- src/main/java/br/com/beholder/compras/api/ApiService.java
+++ src/main/java/br/com/beholder/compras/api/ApiService.java
@@ -3,7 +3,7 @@ package br.com.beholder.compras.api;
 import br.com.beholder.compras.botcore.bots.BotVerificarPrecoDeProduto;
 import br.com.beholder.compras.api.dtos.RelatorioDeProdutosDTO;
 import br.com.beholder.compras.api.dtos.SolicitacaoDeBuscaDTO;
-import br.com.beholder.compras.utils.Stopwatch;
+import br.com.beholder.compras.botcore.utils.Stopwatch;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +16,14 @@ public class ApiService {
         RelatorioDeProdutosDTO relatorioDeProdutosDTO = executarBotVerificarPrecoDeProduto(solicitacao);
 
         cronometroDeExecucao.finalizar();
+        System.out.println(cronometroDeExecucao.toString());
+
         return relatorioDeProdutosDTO;
     }
 
     private RelatorioDeProdutosDTO executarBotVerificarPrecoDeProduto(SolicitacaoDeBuscaDTO solicitacao) {
         BotVerificarPrecoDeProduto bot = new BotVerificarPrecoDeProduto(new ChromeDriver());
-        RelatorioDeProdutosDTO relatorioDeProdutosDTO = bot.executar(solicitacao);
 
-        return  relatorioDeProdutosDTO;
+        return bot.executar(solicitacao);
     }
 }
